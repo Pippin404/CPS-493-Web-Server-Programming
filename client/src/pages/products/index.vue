32b9481a8923cb764d423e6865fae39b6783a076
Pippin404 <Baconpippin@gmail.com>
2024-03-04 15:03:22 -0500

    Added product list and flexible display

---
+++ client/src/pages/products/index.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+    import { type Product, getProducts } from "@/model/products";
+    const products = ref([] as Product[]);
+
+    products.value = getProducts();
+
+</script>
+
+
+
+<template>
+    <div class="product-list">
+                <div v-for="product in products" :key="product.id" class="card">
+        <div class="card-image">
+                <img :src="product.thumbnail" :alt="product.title">
+                <div class="card-content">
+
+                <h3>{{ product.title }}</h3>
+                <h3>{{ product.description }}</h3>
+                <h3>{{ product.price }}</h3>
+            </div>
+        </div>
+    </div>
+    </div>    
+</template>
+
+<style scoped>
+
+    .card {
+
+        flex-basis: 15rem;
+        flex-grow: 1;
+        margin: .5rem;
+
+    }
+
+    .product-list {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+
+</style>
--- client/typed-router.d.ts
+++ client/typed-router.d.ts
@@ -21,5 +21,6 @@ declare module 'vue-router/auto-routes' {
     '/About': RouteRecordInfo<'/About', '/About', Record<never, never>, Record<never, never>>,
     '/documentation': RouteRecordInfo<'/documentation', '/documentation', Record<never, never>, Record<never, never>>,
     '/Index': RouteRecordInfo<'/Index', '/Index', Record<never, never>, Record<never, never>>,
+    '/products/': RouteRecordInfo<'/products/', '/products', Record<never, never>, Record<never, never>>,
   }
 }
